b7cd83c9c5cd895507e9c977cecae6998fd6048a
pangratz <cmueller.418@gmail.com>
2016-08-07 15:27:55 +0200

    Implement initializer so Ember Data is patched when app is loaded

---
+++ addon/initializers/ember-data-store-push-improvements.js
@@ -0,0 +1,10 @@
+import "ember-data-store-push-improvements";
+
+export function initialize(/* application */) {
+  // patches are applied by importing ember-data-store-push-improvements
+}
+
+export default {
+  name: 'ember-data-store-push-improvements',
+  initialize
+};
---
+++ app/initializers/ember-data-store-push-improvements.js
@@ -0,0 +1,1 @@
+export { default, initialize } from 'ember-data-store-push-improvements/initializers/ember-data-store-push-improvements';
